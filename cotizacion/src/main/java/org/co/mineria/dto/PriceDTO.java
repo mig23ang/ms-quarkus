16e2f96022b882043cf493587c1b362bf72edc7f
Miguel Angel Reyes <trc_miguel.reyes@bancompartir.co>
2023-06-26 12:06:47 -0500

    solucion error

--- cotizacion/src/main/java/org/co/mineria/dto/PriceDTO.java
+++ cotizacion/src/main/java/org/co/mineria/dto/PriceDTO.java
@@ -1,13 +1,10 @@
 package org.co.mineria.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.extern.jackson.Jacksonized;
 
-@Jacksonized
-@Builder
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
