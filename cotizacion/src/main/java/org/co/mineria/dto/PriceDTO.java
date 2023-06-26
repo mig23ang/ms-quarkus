f45d04f116d94a290ecce0ca018ff7915efd1689
Miguel Angel Reyes <trc_miguel.reyes@bancompartir.co>
2023-06-26 08:25:03 -0500

    entrega a aldo

--- cotizacion/src/main/java/org/co/mineria/dto/PriceDTO.java
+++ cotizacion/src/main/java/org/co/mineria/dto/PriceDTO.java
@@ -1,11 +1,13 @@
 package org.co.mineria.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
 
 @Jacksonized
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
